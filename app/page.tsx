1185bd24de28d8205bc22a3257d88e87d3960bda
suraj jaisal <surajjaisal445@gmail.com>
2023-11-13 08:01:56 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <h1 className="text-slate-500">hello next</h1>
+    </div>
+  );
+};
+
+export default page;
